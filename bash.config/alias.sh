bd6105add15a491068cc826618680a837befc94e
Anshul Jain <ajain@freelancer.com>
2017-12-04 12:44:08 +1100

    Rename neovim.config to nvim.config

--- bash.config/alias.sh
+++ bash.config/alias.sh
@@ -3,3 +3,9 @@ alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Crese
 alias v='nvim `fzf`'
 alias vim='nvim'
 alias vi='nvim'
+
+alias gfh='git-file-history'
+git-file-history() {
+		for r in `git rev-list --all -- $1 | head -$2 `; do; git --no-pager show --unified=0 --pretty=fuller $r ; done;
+}
+
